d1d2e1579f2ffdd9da51f334bd26ee530e9b5994
Taiki <taiki@TaikinoMacBook-Pro.local>
2022-04-12 18:12:23 +0900

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,15 @@
+// Full screen navigation
+
+$(function(){
+
+    $('.openNav').click(function(){
+        $('#myNav').css('width','100%');
+    });
+
+    $('.closeNav').click(function(){
+        $('#myNav').css('width','0');
+    });
+
+
+
+});
